a9b7fd8fa5d12f61a3e5214824ae671fa12c0f71
Imran Pasha <imranpasha8225@gmail.com>
2025-11-21 22:29:42 +0530

    added pwa

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
 import { defineConfig } from 'vite'
 import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
@@ -61,16 +64,16 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor';
-            }
-            if (id.includes('@mui') || id.includes('@emotion')) {
-              return 'mui';
-            }
-          }
-        },
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+        //       return 'vendor';
+        //     }
+        //     if (id.includes('@mui') || id.includes('@emotion')) {
+        //       return 'mui';
+        //     }
+        //   }
+        // },
       },
     },
     chunkSizeWarningLimit: 1000, // Increase warning limit to 1000kb
