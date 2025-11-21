d73c648fcae01720225d452ee87cafc2a521f130
Imran Pasha <imranpasha8225@gmail.com>
2025-11-21 20:52:39 +0530

    pwa 2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.731364qh53o"
+    "revision": "0.9ig3b7dg6j"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
